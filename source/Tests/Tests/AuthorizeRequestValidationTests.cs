b50ba3f6790fcc8fcd527a237ad1d78ace5ffd79
Dominick Baier <dbaier@leastprivilege.com>
2013-05-12 17:03:16 +0200

    Added Test project

---
+++ source/Tests/Tests/AuthorizeRequestValidationTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Thinktecture.AuthorizationServer.Test
+{
+    [TestClass]
+    public class AuthorizeRequestValidationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

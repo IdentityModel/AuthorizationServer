540394d2d21f8432d7ace4a3a7541201840b77d2
Brock Allen <brockallen@gmail.com>
2013-05-27 15:15:49 -0400

    added GlobalConfiguration for server-wide settings

---
+++ source/Libraries/Thinktecture.AuthorizationServer.Core/Models/GlobalConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thinktecture.AuthorizationServer.Models
+{
+    // highlander class
+    public class GlobalConfiguration
+    {
+        public string AuthorizationServerName { get; set; }
+        public string AuthorizationServerLogoUrl { get; set; }
+        public string Issuer { get; set; }
+
+        public List<string> Administrators { get; set; }
+    }
+}

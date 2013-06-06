5a6499996312c83163f19e2f0da904f712ccecea
Brock Allen <brockallen@gmail.com>
2013-06-06 17:23:48 -0400

    add account controller for signout

---
+++ source/WebHost/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Services;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Thinktecture.AuthorizationServer.WebHost.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult SignOut()
+        {
+            FederatedAuthentication.WSFederationAuthenticationModule.SignOut();
+            return Redirect("~");
+        }
+
+    }
+}

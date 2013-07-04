3579c1cd7a4a5f4ba9c660cfcfc8d26fcffe7754
Brock Allen <brockallen@gmail.com>
2013-07-04 17:32:52 -0400

    updated nugets

--- source/WebHost/Scripts/_references.js
+++ source/WebHost/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-2.0.1.js" />
+﻿/// <reference path="jquery-2.0.2.js" />
 /// <reference path="jquery-ui-1.10.3.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />

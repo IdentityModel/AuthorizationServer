d7d416b8984efc3b4e1266285b15a99c2a178827
Dominick Baier <dbaier@leastprivilege.com>
2013-09-30 18:57:33 +0200

    Added new OAuth2Client

---
+++ samples/Flows/Thinktecture.IdentityModel.Clients.Preview/BasicAuthenticationHeaderValue.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see license.txt
+ */
+
+using System;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Thinktecture.IdentityModel.Clients.Preview
+{
+    public class BasicAuthenticationHeaderValue : AuthenticationHeaderValue
+    {
+        public BasicAuthenticationHeaderValue(string userName, string password)
+            : base("Basic", EncodeCredential(userName, password))
+        { }
+
+        private static string EncodeCredential(string userName, string password)
+        {
+            Encoding encoding = Encoding.UTF8;  //Encoding.GetEncoding("iso-8859-1");
+            string credential = String.Format("{0}:{1}", userName, password);
+
+            return Convert.ToBase64String(encoding.GetBytes(credential));
+        }
+    }
+}
---
+++ samples/Flows/Thinktecture.IdentityModel.Clients.Preview/TokenResponse.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json.Linq;
+using Thinktecture.IdentityModel.Constants.Preview;
+
+namespace Thinktecture.IdentityModel.Clients.Preview
+{
+    public class TokenResponse
+    {
+        public string Raw { get; protected set; }
+        public JObject Json { get; protected set; }
+
+        public TokenResponse(string raw)
+        {
+            Raw = raw;
+            Json = JObject.Parse(raw);
+        }
+
+        public string AccessToken
+        {
+            get
+            {
+                return GetStringOrNull(OAuth2Constants.AccessToken);
+            }
+        }
+
+        public long ExpiresIn
+        {
+            get
+            {
+                return GetLongOrNull(OAuth2Constants.ExpiresIn);
+            }
+        }
+
+        public string TokenType
+        {
+            get
+            {
+                return GetStringOrNull(OAuth2Constants.TokenType);
+            }
+        }
+
+        public string RefreshToken
+        {
+            get
+            {
+                return GetStringOrNull(OAuth2Constants.RefreshToken);
+            }
+        }
+
+        protected virtual string GetStringOrNull(string name)
+        {
+            var value = Json[name];
+
+            if (name != null)
+            {
+                return value.ToString();
+            }
+
+            return null;
+        }
+
+        protected virtual long GetLongOrNull(string name)
+        {
+            var value = Json[name];
+
+            if (name != null)
+            {
+                long longValue = 0;
+                if (long.TryParse(value.ToString(), out longValue))
+                {
+                    return longValue;
+                }
+            }
+
+            return 0;
+        }
+    }
+}

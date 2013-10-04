42a046f43615cb4432be6b17849e487fff18e39a
Dominick Baier <dbaier@leastprivilege.com>
2013-10-04 11:02:45 +0200

    Added extension methods

---
+++ samples/Flows/Thinktecture.IdentityModel.Clients.Preview/HttpClientExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+
+namespace Thinktecture.IdentityModel.Clients.Preview.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static void SetBasicAuthentication(this HttpClient client, string userName, string password)
+        {
+            client.DefaultRequestHeaders.Authorization = new BasicAuthenticationHeaderValue(userName, password);
+        }
+
+        public static void SetToken(this HttpClient client, string scheme, string token)
+        {
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(scheme, token);
+        }
+
+        public static void SetBearerToken(this HttpClient client, string token)
+        {
+            client.SetToken("Bearer", token);
+        }
+    }
+}

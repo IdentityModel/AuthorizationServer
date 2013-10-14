72588c46f6de1b99f024ed7352c677662f03112f
Dominick Baier <dbaier@leastprivilege.com>
2013-10-14 19:15:06 +0200

    Added header and query string support

---
+++ samples/Flows/ResourceServer (Web API v2)/IdentityModel.Katana/HeaderOAuthBearerProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Threading.Tasks;
+
+namespace Thinktecture.IdentityModel.Owin
+{
+    public class HeaderOAuthBearerProvider : OAuthBearerAuthenticationProvider
+    {
+        readonly string _name;
+
+        public HeaderOAuthBearerProvider(string name)
+        {
+            _name = name;
+        }
+
+         public override async Task RequestToken(OAuthRequestTokenContext context)
+        {
+            var value = context.Request.Headers.Get(_name);
+
+            if (!string.IsNullOrEmpty(value))
+            {
+                context.Token = value;
+            }
+        }
+    }
+}
---
+++ samples/Flows/ResourceServer (Web API v2)/IdentityModel.Katana/QueryStringOAuthBearerProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Threading.Tasks;
+
+namespace Thinktecture.IdentityModel.Owin
+{
+    public class QueryStringOAuthBearerProvider : OAuthBearerAuthenticationProvider
+    {
+        readonly string _name;
+
+        public QueryStringOAuthBearerProvider(string name)
+        {
+            _name = name;
+        }
+
+        public override async Task RequestToken(OAuthRequestTokenContext context)
+        {
+            var value = context.Request.Query[_name];
+
+            if (!string.IsNullOrEmpty(value))
+            {
+                context.Token = value;
+            }
+        }
+    }
+}

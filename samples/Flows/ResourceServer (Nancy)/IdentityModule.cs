552e7cf5a9f936d51672b36769bd000ca007c44f
Dominick Baier <dbaier@leastprivilege.com>
2013-10-25 10:40:34 +0200

    More cleanup

--- samples/Flows/ResourceServer (Nancy)/IdentityModule.cs
+++ samples/Flows/ResourceServer (Nancy)/IdentityModule.cs
@@ -18,15 +18,14 @@ namespace ResourceServer
                         return HttpStatusCode.Unauthorized;
                     }
 
-                    var claims = new List<ViewClaim>(
-                        from c in principal.Claims
-                        select new ViewClaim
-                        {
-                            Type = c.Type,
-                            Value = c.Value
-                        });
+                    var claims = from c in principal.Claims
+                                 select new ViewClaim
+                                 {
+                                     Type = c.Type,
+                                     Value = c.Value
+                                 };
 
-                    return Response.AsJson<List<ViewClaim>>(claims);
+                    return Response.AsJson<IEnumerable<ViewClaim>>(claims);
                 };
         }
     }
